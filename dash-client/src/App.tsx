941c6b749ba99a3e9cde59c305458d26e41953e4
Tiger-Foxx <donfackarthur750@gmail.com>
2025-02-04 18:16:23 +0100

    first commit

---
+++ dash-client/src/App.tsx
@@ -0,0 +1,11 @@
+import VideoPlayer from "./components/VideoPlayer.jsx";
+
+function App() {
+  return (
+    <div>
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
---
+++ dash-client/src/components/VideoPlayer.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+import dashjs from "dashjs";
+
+const VideoPlayer = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const player = dashjs.MediaPlayer().create();
+      player.initialize(videoRef.current, "http://192.168.100.208:3000/manifest.mpd", true);
+    }
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Lecteur DASH avec React</h1>
+      <video ref={videoRef} controls autoPlay style={{ width: "80%", border: "2px solid black" }} />
+    </div>
+  );
+};
+
+export default VideoPlayer;
+
+/***
+ ✅ Backend Express qui sert la vidéo.
+ ✅ Frontend React avec Vite qui affiche la vidéo.
+ ✅ Stream en DASH fonctionnel.
+ **/
